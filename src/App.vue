42e3c57ae89d4a4649a2cfb8f1de55088efe03c1
gab <corbanigabriele@gmail.com>
2024-04-10 16:13:20 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+// primo passaggio imporare il componente
+import HeroSection from './components/HeroSection.vue'
+import ProductSection from './components/ProductSection.vue'
+
+export default {
+  // secondo passaggio registrare il componente
+  components: {
+    HeroSection,
+    ProductSection
+    
+  },
+  data() {
+    return {
+      message: 'Hello world!'
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Utilizzare il componente nel template -->
+  <HeroSection />
+  <ProductSection />
+</template>
+
+<!-- qui non ci deve essere l'attributo scoped -->
+<style lang="scss">
+// ultimo passaggio importaimo il file general con gli stili globali
+@use './style/general';
+
+</style>
---
+++ src/components/HeroSection.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  data() {
+    return {
+    //   src: 'vite.svg'
+    }
+  }
+}
+</script>
+
+<template>
+  <section class="page-header">
+    <nav class="container">
+          <div class="row">
+            <ul class="flex">
+              <li>Donna</li>
+              <li>Uomo</li>
+              <li>Bambini</li>
+            </ul>
+  
+            <a class="logo" href="#">
+              <img src="/img/boolean-logo.png" alt="" />
+            </a>
+  
+            <ul class="flex">
+              <li>&hearts;</li>
+              <li>&hearts;</li>
+              <li>&hearts;</li>
+            </ul>
+          </div>
+        </nav>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.page-header {
+	background-color: #fe6900;
+	color: white;
+	padding: 16px 0;
+	box-shadow: 0px 0px 20px 2px rgba(0, 0, 0.1);
+}
+
+.row {
+	justify-content: space-between;
+  align-items: center;
+  
+  ul {
+    gap: 20px;
+  }
+}
+
+.logo img {
+	height: 32px;
+}
+
+</style>
